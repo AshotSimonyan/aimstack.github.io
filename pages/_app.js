5f57de634b6c13393f932234a2cdbabb98a7684c
AshotUltraLabs <a.simonyan.stdev@gmail.com>
2022-11-14 14:51:48 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,15 +6,22 @@ import { DefaultSeo } from 'next-seo';
 import SEO from '../next-seo.config';
 
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return ( <>
     <Head>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
     </Head>
     <DefaultSeo {...SEO} />
-    <Header/>
+      {
+          !router?.state?.route === '/admin' &&
+          <Header/>
+      }
+
     <Component {...pageProps} />
-    <Footer />
+      {
+          !router?.state?.route === '/admin' &&
+          <Footer />
+      }
   </>)
 }
 
